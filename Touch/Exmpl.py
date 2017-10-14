07c4ac29b57a65e6466d0109a40341e501c2a297
ME <michael.erdl95@gmail.com>
2017-10-15 01:54:42 +0200

    i kinda have no idea what I am doing

---
+++ Touch/Exmpl.py
@@ -0,0 +1,10 @@
+import mraa
+print (mraa.getVersion())
+touch = mraa.Gpio(29)
+touch.dir(mraa.DIR_IN)
+while True:
+  touchButton = int(touch.read())
+  if(touchButton == 1):
+  print('On')
+  else:
+  Print('Off')
