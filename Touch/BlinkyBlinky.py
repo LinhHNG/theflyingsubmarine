8d55fa88e4a011a2e932d1ca9ea998674d03f26e
ME <michael.erdl95@gmail.com>
2017-10-14 23:06:52 +0200

    rr

--- Touch/BlinkyBlinky.py
+++ Touch/BlinkyBlinky.py
@@ -1,6 +1,14 @@
-import mraa
+import mraa,time
 
 print (mraa.getVersion())
-i= 29
-touch = mraa.Gpio(i)
-touch.dir(mraa.DIR_OUT)
+i= 0
+while True:
+    try:
+        t = mraa.Gpio(i)
+        t.dir(mraa.DIR_OUT)
+    except:
+        print('Error on Pin '+str(i))
+    else:
+        print('Works on '+str(i))
+
+    
