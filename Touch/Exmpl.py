c922ce086a88adc7671cd704cbafd8785d13cc92
ME <michael.erdl95@gmail.com>
2017-10-15 02:04:18 +0200

    Buttons!!!

--- Touch/Exmpl.py
+++ Touch/Exmpl.py
@@ -1,10 +1,14 @@
 import mraa
+oldState = 0
 print (mraa.getVersion())
 touch = mraa.Gpio(29)
 touch.dir(mraa.DIR_IN)
+print('Off')
 while True:
-  touchButton = int(touch.read())
-  if(touchButton == 1):
-      print('On')
+    touchButton = int(touch.read())
+    if(oldState == 0):
+        if(touchButton == 1):
+            print('On')
   else:
-      print('Off')
+      if(touchButton == 1):
+          print('Off')
