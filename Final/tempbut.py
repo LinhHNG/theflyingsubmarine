0eb448eedaa9f76dc8cc5cfef281be86f8fb1b4c
ME <michael.erdl95@gmail.com>
2017-10-15 12:49:15 +0200

    pity py

--- Final/tempbut.py
+++ Final/tempbut.py
@@ -9,28 +9,27 @@ ard = serial.Serial('/dev/tty96B0', 9600)
 
 
 def showTemp(temp):
-	if temp >= 30
+	if temp >= 30:
 		print('help it is '+str(temp)+ 'Degrees')
 
 if __name__ == '__main__':
-
-	try:
-		while True:
-			touchButton = int(touch.read())
-		    if(oldState == 0):
-		        if(touchButton == 1):
-		   			print('help')
-		            oldState = 1
-		    else:
-		        if(touchButton == 0):
-		            oldState = 0
-			ardOut = ard.read()
-			ardTemp = ardOut.split('__*')
-			while '' in showTemp:
-				showTemp.remove('')
-			Temp = ''.join(showTemp)
-			Temp = int(Temp)
-			showTemp(Temp)
-	except KeyboardInterrupt:
-		print("CTRL-C!! Exiting...")
+    try:
+        while True:
+            touchButton = int(touch.read())
+            if(oldState == 0):
+                if(touchButton == 1):
+                    print('help')
+                    oldState = 1
+            else:
+                if(touchButton == 0):
+                    oldState = 0
+            ardOut = ard.read()
+            ardTemp = ardOut.split('__*')
+            while '' in showTemp:
+                showTemp.remove('')
+                Temp = ''.join(showTemp)
+                Temp = int(Temp)
+            showTemp(Temp)
+    except KeyboardInterrupt:
+        print("CTRL-C!! Exiting...")
 		
